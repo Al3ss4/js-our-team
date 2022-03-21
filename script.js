d6ad2468d1a98bd6135b2479d2e609e37662d4c2
Mario Rovito <alessandro.r14@gmail.com>
2022-03-21 15:17:50 +0100

    add "array oggetti "

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
 console.log('JS OK!')
+
+
+
+//Aggiungere un file js in cui definire *un array di oggetti* che rappresentano i membri del team. Ogni membro ha le informazioni necessarie per stampare la relativa card: *Nome, Ruolo e Foto*.
+
+
+
+const teamCard = [
+    {
+        'nome': 'Angela Caroll',
+        'ruolo': 'Chief Editor',
+        'foto': 'angela-caroll-chief-editor.jpg',
+    },
+
+    {
+        'nome': 'Angela Lopez',
+        'foto': 'Social Media Manager',
+        'ruolo': 'angela-lopez-social-media-manager.jpg',
+        
+    },
+
+    {
+        'nome': 'Barbara Ramos',
+        'foto': 'Graphic Designer',
+        'ruolo': 'barbara-ramos-graphic-designer.jpg', 
+    },
+
+    {
+        'nome': 'Scott Estrada',
+        'ruolo': 'Developer',
+        'foto': 'scott-estrada-developer.jpg',
+    },
+
+    {
+        'nome': 'Walter Gordon',
+        'ruolo': 'Office Manager',
+        'foto': 'walter-gordon-office-manager.jpg',
+    },
+
+    {
+        'nome': 'Wayne Barnett',
+        'ruolo': 'Founder & CEO',
+        'foto': 'wayne-barnett-founder-ceo.jpg',
+    }
+];
+
+console.log(teamCard);
