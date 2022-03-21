195f772ce483c2f009bf6508ce535140c3418b6a
Mario Rovito <alessandro.r14@gmail.com>
2022-03-21 15:56:58 +0100

    add function and fix code

--- script.js
+++ script.js
@@ -1,48 +1,63 @@
-console.log('JS OK!')
+console.log('JS OK!');
 
-
+printCard();
 
 //Aggiungere un file js in cui definire *un array di oggetti* che rappresentano i membri del team. Ogni membro ha le informazioni necessarie per stampare la relativa card: *Nome, Ruolo e Foto*.
 
 
 
-const teamCard = [
-    {
-        'nome': 'Angela Caroll',
-        'ruolo': 'Chief Editor',
-        'foto': 'angela-caroll-chief-editor.jpg',
-    },
 
-    {
-        'nome': 'Angela Lopez',
-        'foto': 'Social Media Manager',
-        'ruolo': 'angela-lopez-social-media-manager.jpg',
-        
-    },
 
-    {
-        'nome': 'Barbara Ramos',
-        'foto': 'Graphic Designer',
-        'ruolo': 'barbara-ramos-graphic-designer.jpg', 
-    },
+//creo una funzione " getCard"
 
-    {
-        'nome': 'Scott Estrada',
-        'ruolo': 'Developer',
-        'foto': 'scott-estrada-developer.jpg',
-    },
+function getCard (){
+    return teamCard = [
+        {
+            'nome': 'Angela Caroll',
+            'ruolo': 'Chief Editor',
+            'foto': 'angela-caroll-chief-editor.jpg',
+        },
+    
+        {
+            'nome': 'Angela Lopez',
+            'foto': 'Social Media Manager',
+            'ruolo': 'angela-lopez-social-media-manager.jpg',
+            
+        },
+    
+        {
+            'nome': 'Barbara Ramos',
+            'foto': 'Graphic Designer',
+            'ruolo': 'barbara-ramos-graphic-designer.jpg', 
+        },
+    
+        {
+            'nome': 'Scott Estrada',
+            'ruolo': 'Developer',
+            'foto': 'scott-estrada-developer.jpg',
+        },
+    
+        {
+            'nome': 'Walter Gordon',
+            'ruolo': 'Office Manager',
+            'foto': 'walter-gordon-office-manager.jpg',
+        },
+    
+        {
+            'nome': 'Wayne Barnett',
+            'ruolo': 'Founder & CEO',
+            'foto': 'wayne-barnett-founder-ceo.jpg',
+        }
+    ];
+    
+}
 
-    {
-        'nome': 'Walter Gordon',
-        'ruolo': 'Office Manager',
-        'foto': 'walter-gordon-office-manager.jpg',
-    },
 
-    {
-        'nome': 'Wayne Barnett',
-        'ruolo': 'Founder & CEO',
-        'foto': 'wayne-barnett-founder-ceo.jpg',
+//creo funzione per stamapre le carte
+function printCard(){
+    let carte= getCard();
+    for (let i=0; i < carte.length; i++){
+
+//console.log(carte[i]);
     }
-];
-
-console.log(teamCard);
+}
